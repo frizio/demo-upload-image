e8ae8145fe213c57bff564f0a6a8dc705b7017b8
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-10 16:02:55 +0200

    Add image upload callback (basic code)

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('Javascript work!!');
+
+const imagePreview =  document.getElementById('img-preview');
+
+const imageUploader = document.getElementById('img-uploader');
+
+imageUploader.addEventListener(
+    'change',
+    (e) => {
+        console.log('Image selected');
+        console.log(e);
+    }
+);
