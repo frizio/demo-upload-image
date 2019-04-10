7344f8de6c7b435e03302a55fe03127803557f01
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-10 16:23:41 +0200

    Add Claudinary url and preset

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const imagePreview =  document.getElementById('img-preview');
 
 const imageUploader = document.getElementById('img-uploader');
 
+const CLAUDINARY_URL = 'https://api.cloudinary.com/v1_1/frizio/image/upload';
+
+const CLAUDINARY_PRESET = 'yzsmlfl9';
+
 imageUploader.addEventListener(
     'change',
     (e) => {
-        console.log('Image selected');
-        console.log(e);
+        const theFile = e.target.files[0];
+        console.log(theFile);
     }
 );
